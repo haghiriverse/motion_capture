8271a04b3d8cee325fe34817abbc6fed4cad6432
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-26 01:13:32 +0430

    Error handling for better purposes, eh?

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ detector = PoseDetector()
 while video_src.isOpened():
     _, frame = video_src.read()
 
-    frame = detector.findPose(frame)
-
     try:
+        frame = detector.findPose(frame)
+
         cv2.imshow("Video", frame)
     except Exception as e:
         print(e)
         break
-    
+
     cv2.waitKey(1)
