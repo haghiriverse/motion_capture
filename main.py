cfcd8048ac07f9721baf782e24849b023dd3b709
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-26 13:31:04 +0430

    Again changed the video, not a big deal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 
 from cvzone.PoseModule import PoseDetector
 
-video_src = cv2.VideoCapture("video_3.mp4")
+video_src = cv2.VideoCapture("video.mp4")
 
 detector = PoseDetector()
 posList  = []
